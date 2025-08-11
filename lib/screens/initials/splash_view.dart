7ad72c227123da414d04d331e1252525d0210f37
ehsan <ehsanyaqoob07@gmail.com>
2025-08-11 12:52:25 +0500

    just checking up the project

--- lib/screens/initials/splash_view.dart
+++ lib/screens/initials/splash_view.dart
@@ -6,7 +6,7 @@ class SplashView extends StatefulWidget {
   @override
   State<SplashView> createState() => _SplashViewState();
 }
-
+  
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
